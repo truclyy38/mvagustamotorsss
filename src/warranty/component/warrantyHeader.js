ec1553605d53d0c66f2aab2e7d024796d5a1fb76
truclyy38 <truclyy38@gmail.com>
2021-04-11 23:57:37 +0700

    Update My Warranty Page
    - Rereponsive to adjust the pictures
    - change the max-height to resolve one bug

--- src/warranty/component/warrantyHeader.js
+++ src/warranty/component/warrantyHeader.js
@@ -1,12 +1,15 @@
 import React from 'react'
 export function WarrantyHeader(){
     return(
-        <div className="wr-header-content">
+        <div className="wr-hd-contain">
+ <div className="wr-header-content">
             <div className="wr-header-title">
             MV AGUSTA
             <br></br>
             WARRANTY
             </div>
         </div>
+        </div>
+       
     );
 }
